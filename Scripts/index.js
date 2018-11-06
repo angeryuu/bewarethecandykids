4f1a4aec334ad73ff0cf0c93c356430e548cd4fb
TeamAmeba <43379282+TeamAmeba@users.noreply.github.com>
2018-11-06 15:10:20 +0100

    Add files via upload

--- Scripts/index.js
+++ Scripts/index.js
@@ -13,34 +13,38 @@ window.onload = function(){
         canvasTop = canvas.offsetTop;
         ctx = canvas.getContext('2d');
 
+        pj = new Jugador(canvas);
+
         canvas.addEventListener('click', function(event) {
             var x = event.pageX - canvasLeft,
                 y = event.pageY - canvasTop;
-                var dir = [0,0];
-                dir.x = x-pj.x;
-                dir.y = y-pj.y;
-                var mod = Math.sqrt(x*x + y*y);
-                dir.x = dir.x/mod;
-                dir.y = dir.y/mod;
-                caramelos.push(new Caramelo(canvas, dir));
-            console.log('x:' + x + ' y:' + y);
+            var dir = [0,0];
+            dir.x = x-pj.x - pj.sprite.width/2;
+            dir.y = y-pj.y - pj.sprite.height/2;
+            var mod = Math.sqrt((x*x) + (y*y));
+            dir.x = dir.x/mod;
+            dir.y = dir.y/mod;
+            //console.log(dir.x + " " + dir.y);
+            caramelos.push(new Caramelo(canvas, dir, pj.x, pj.y));
         }, false);
+        
 
-        pj = new Image();
-        pj.onload = function () {
-            ctx.drawImage(pj, 400-pj.width/2, 300-pj.height/2);
-        }
-        pj.src = "Sprites/Monstro_Final.png";
     }
 
     function update(progress) {
-        caramelos.forEach( function(){
+        caramelos.forEach( function(valor, i, array){
             caramelos[i].update(progress);
         });
     }
       
     function draw() {
-        
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        pj.draw();
+
+        caramelos.forEach( function(valor, i, array){
+            caramelos[i].draw();
+        });
     }
       
     function loop(timestamp) {
--- Scripts/jugador.js
+++ Scripts/jugador.js
@@ -1,15 +1,14 @@
-function Caramelo(canvas, _dir, _x, _y){
+function Jugador(canvas){
 
     this.draw = function (){
         this.ctx.drawImage(this.sprite, this.x, this.y);
     }
 
     this.ctx = canvas.getContext('2d');
-    this.dir = _dir;
     this.sprite = new Image();
     this.sprite.src = "Sprites/Monstro_Final.png";
-    this.x = _x;
-    this.y = _y;
+    this.x = canvas.width/2-214/2;
+    this.y = canvas.height/2-209/2;
 
     this.update = function (progress) {
         this.x += this.dir.x * progress;
