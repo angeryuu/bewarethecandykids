319da326e9b3872fc4dedb5c0db3bbea52114699
TeamAmeba <43379282+TeamAmeba@users.noreply.github.com>
2018-12-05 00:43:34 +0100

    Thorw añadido

--- Scripts/gameState.js
+++ Scripts/gameState.js
@@ -118,6 +118,7 @@ function gameState(container){
             candySound.play();
             caramelos.push(new Caramelo(canvas, newKid, dir, pj.x + pj.sprite.width/2, pj.y + pj.sprite.height/2));
         }
+        pj.throw();
     }
 
     create();
